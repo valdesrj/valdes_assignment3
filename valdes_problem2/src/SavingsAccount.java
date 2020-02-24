f01e7c1f35af6451e74a53186e4857db356e9f90
Richard Valdes <richardj.valdes@gmail.com>
2020-02-23 23:32:35 -0500

    completes Problem 2 SavingAccount

--- valdes_problem2/src/SavingsAccount.java
+++ valdes_problem2/src/SavingsAccount.java
@@ -1,4 +1,58 @@
 
+// class 
 public class SavingsAccount {
-
+    private static double annualInterestRate; // for all account holders
+    private double savingsBalance; // amount the saver currently has on deposit
+    private int saverID;
+    
+    // constructor
+    public SavingsAccount(int saverID, double savingsBalance) {
+    	if (savingsBalance < 0) {
+    		throw new IllegalArgumentException ("Balance must be >= 0");
+    	}
+    	
+    	this.savingsBalance = savingsBalance;
+    	this.saverID = saverID;
+    }
+    
+    // set the savings balance
+    public void setSavingsBalance(double savingsBalance) {
+    	if (savingsBalance < 0) {
+    		throw new IllegalArgumentException ("Balance must be >= 0");
+    	}
+    	
+    	this.savingsBalance = savingsBalance;
+    }
+    
+    // modify the interest rate
+    public static void modifyInterestRate(double interestRate) {
+    	annualInterestRate = interestRate;
+    }
+    
+    // calculate the monthly interest
+    public double calculateMonthlyInterest() {
+    	return getSavingsBalance() * getAnnualInterestRate() /  12.;
+    }
+    
+    // return the ID
+    public int getSaverID() {return saverID;}
+    
+    // return the savings balance
+    public double getSavingsBalance() {
+    	return savingsBalance;
+    }
+    
+    // return the annual interest rate
+    public double getAnnualInterestRate() {
+    	return annualInterestRate;
+    }
+    
+    @Override
+    public String toString() {
+    	return String.format("%s%d %s: $%,.2f", "Saver ", getSaverID(), "Current Balance", getSavingsBalance());
+    }
+    
+    
+    
+    
 }
--- valdes_problem2/src/SavingsAccountTest.java
+++ valdes_problem2/src/SavingsAccountTest.java
@@ -1,9 +1,40 @@
 
+// Test class to test class SavingsAccount. Instantiate two SavingsAccount
+// objects, saver1 and saver2, with balances of $2000.00 and $3000.00,
+// respectively. Set annualInterestRate to 4%, then calculate the monthly
+// interest for each of 12 months and print the monthly balances for both
+// savers. Next, set the annualInterestRate to 5%, calculate the next month’s
+// interest and print the new balances for both savers.
+
 public class SavingsAccountTest {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// create savings accounts
+		SavingsAccount saver1 = new SavingsAccount(1, 2000.00);
+		SavingsAccount saver2 = new SavingsAccount(2, 3000.00);
+		
+		// set the interest rate to 4%
+		SavingsAccount.modifyInterestRate(0.04);
+	
+		// output the monthly balances for each saver
+		System.out.printf("%s%n", "Opening Balances");
+		System.out.printf("%s, %s%n", saver1, saver2);
+		System.out.println("======================================================================================");
+		System.out.printf("Interest rate set to %.2f%n", saver1.getAnnualInterestRate());
+		for (int i = 0; i < 12; i++) {
+			// calculate the monthly interest		
+			// add the monthly interest to the savings balance
+			saver1.setSavingsBalance(saver1.getSavingsBalance() + saver1.calculateMonthlyInterest());
+			saver2.setSavingsBalance(saver2.getSavingsBalance() + saver2.calculateMonthlyInterest());
+			System.out.printf("Month #%d: %s, %s%n", i+1, saver1, saver2);
+		}
+		
+		SavingsAccount.modifyInterestRate(0.05);
+		System.out.printf("%nIncrease the interest rate to %.2f%n", saver1.getAnnualInterestRate());
+		System.out.println("======================================================================================");
+		// set the interest rate to 5%
+		saver1.setSavingsBalance(saver1.getSavingsBalance() + saver1.calculateMonthlyInterest());
+		saver2.setSavingsBalance(saver2.getSavingsBalance() + saver2.calculateMonthlyInterest());
+		System.out.printf("%s, %s%n", saver1, saver2);
 	}
 
 }
